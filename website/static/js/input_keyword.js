e7e10f4efe2685cc3ab10c8bbc6d387aba9c0d33
jungry <seojungrain@gmail.com>
2022-12-31 16:50:59 +0900

    jungry

--- website/static/js/input_keyword.js
+++ website/static/js/input_keyword.js
@@ -33,8 +33,10 @@ $('li').on('click',function(event){
                 if (user_keyword == select_user_keyword[prop]) {
                     fun_key = user_keyword.replace(' ', '_')
                     select_user.innerHTML += `
-                        <div id="keyword_btn${prop}" class="button-keywords">
-                            <button type="button" class="btn btn-info">${select_user_keyword[prop]}</button>
+                        <div id="keyword_btn${prop}" class="button-keywords d-flex align-items-center mb-3">
+                            <button id="scroll_btn" onclick="scroll_btn(${fun_key})" type="button" class="btn btn-info">
+                                ${select_user_keyword[prop]}
+                            </button>
                             <button onclick="remove_btn(${prop},'${fun_key}')" name="${prop}" type="button" class="btn-close" aria-label="Close"></button>
                         </div>
                     `
@@ -43,15 +45,16 @@ $('li').on('click',function(event){
 
             const list_len = $(response["webtoon_title"]).length
             output.innerHTML += `
-                    <div id="${fun_key}" class="keywords">
+                <div id="${fun_key}" class="keywords">
+                    <p class="mt-3" style="font-size: 20px;"><strong>${user_keyword}</strong></p>
             `
             let keyword_result = document.getElementById(fun_key)
             for (let i = 0; i < list_len; i++) {
                 //전체를 div를 감싼다. 어떤 키워드를 삭제할지 알아야함
                 keyword_result.innerHTML += `
                 <div class="div-result">
-                    <a class="button-result" href="/get_rcm/${response["webtoon_title"][i]}">
-                        <div>
+                    <a class="button-result d-flex align-items-center" href="/get_rcm/${response["webtoon_title"][i]}">
+                        <div class="div-img">
                             <img src='${response["webtoon_thumb"][i]}' style="width:100px; height:100px">
                         </div>  
                         <div class="div-tai">
@@ -179,23 +182,23 @@ $('.addAutoCompleteKeyword').on('click', function (event) {
 
                     keyword_result.innerHTML += `
                     <div class="div-result">
-                    <a class="button-result" href="/get_rcm/${response["webtoon_title"][i]}">
-                        <div>
-                            <img src='${response["webtoon_thumb"][i]}' style="width:100px; height:100px">
-                        </div>  
-                        <div class="div-tai">
-                        <div class="div-title">
-                            <p>${response["webtoon_title"][i]}</p>
-                        </div>
+                        <a class="button-result" href="/get_rcm/${response["webtoon_title"][i]}">
+                            <div class="div-img">
+                                <img src='${response["webtoon_thumb"][i]}' style="width:100px; height:100px">
+                            </div>  
+                            <div class="div-tai">
+                            <div class="div-title">
+                                <p>${response["webtoon_title"][i]}</p>
+                            </div>
 
-                        <div class="div-author">
-                            <p>${response["webtoon_author"][i]}</p>
-                        </div>
-                        <div class="div-intro">
-                            <p>${response["webtoon_intro"][i]}</p>
-                        </div>   
-                        </div>
-                    </a>
+                            <div class="div-author">
+                                <p>${response["webtoon_author"][i]}</p>
+                            </div>
+                            <div class="div-intro">
+                                <p>${response["webtoon_intro"][i]}</p>
+                            </div>   
+                            </div>
+                        </a>
                     </div>
                 `
                 }
@@ -217,3 +220,13 @@ $('.addAutoCompleteKeyword').on('click', function (event) {
 
 })
 
+// 해당 스크롤 이동 : 수정 필요
+function scroll_btn(scroll_index) {
+    let offset = $(scroll_index).offset();
+    $('html,body').animate({scrollTop : offset.top},200)    // 200. 숫자가 클수록 천천히 스크롤 이동
+}
+
+function scroll_up() {
+    let keyword_input = $('#keyword_container').offset();
+    $('html,body').animate({scrollTop : keyword_input.top},200)
+}
